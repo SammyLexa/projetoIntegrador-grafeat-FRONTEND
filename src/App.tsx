21a0f85ef5a28235aa22b4db2a56bbbbf566f9d9
Samantha Alexandra <lexalaska31@gmail.com>
2023-04-14 19:39:31 -0300

    :sparkles:  Mesclando Branch @SammyLexa

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+   <>
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <div style={{ minHeight: '100vh' }}>
+          <Routes> // Antigo Switch
+            {/* <Route path="/" element={<Login />} /> */}
+            {/* 
+            <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/home" element={<Home />} /> */}
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </>
   )
 }
