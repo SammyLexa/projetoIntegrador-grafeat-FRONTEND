8640215d52b7d52dbd51602330a13a18c2b0a1a5
pedroapinheiro <pedro.azevedop18@hotmail.com>
2023-04-14 19:26:50 -0300

    :sparkles: Adicionando Footer e Navbar @pedroapinheiro

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
+import Footer from './components/static/footer/Footer'
+import Navbar from './components/static/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
-      <Footer /> 
     </>
   )
 }
