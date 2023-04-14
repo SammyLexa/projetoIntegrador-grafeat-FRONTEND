4c9f44f4629dfe48cd1c9a050726bebf8fa3ad95
Leticia <leticiapiress1@outlook.com>
2023-04-14 16:12:38 -0300

    :sparkles: Adicionando a página Home @LeticiaPires12

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./assets/pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
     </>
   )
 }
