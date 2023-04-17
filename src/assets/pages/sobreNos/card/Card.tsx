d29aa734413a34a493f8af6725d0c8f2c75ca253
Caroline Viana <carolviana-21@hotmail.com>
2023-04-17 08:07:58 -0300

    :lipstick: Terminando a estilização @Caroline-Viana

--- src/assets/pages/sobreNos/card/Card.tsx
+++ src/assets/pages/sobreNos/card/Card.tsx
@@ -10,15 +10,15 @@ function Card({ nome, cargo, bio, linkedIn, github, img }: { nome: string, cargo
             <Box className='card_info'>
                 <div className='info_secao'>
                     <h2 className='secao_titulo'>{nome}</h2>
-                    <h5>{cargo}</h5>
-                    <p>{bio}</p>
+                    <h5 className="secao_texto">{cargo}</h5>
+                    <p className="secao_texto">{bio}</p>
                 </div>
                 <div className='info_link'>
                     <a href={linkedIn} target="_blank">
-                        <LinkedInIcon style={{ fontSize: 30, color: "black" }} />
+                        <LinkedInIcon style={{ fontSize: 30, color: "white" }} />
                     </a>
                     <a href={github} target="_blank">
-                        <GitHubIcon style={{ fontSize: 30, color: "black" }}></GitHubIcon>
+                        <GitHubIcon style={{ fontSize: 30, color: "white" }}></GitHubIcon>
                     </a>
                 </div>
             </Box>
