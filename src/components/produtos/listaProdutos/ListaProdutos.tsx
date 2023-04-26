3e70610655505dc51fe5d7b03209f22df79830c7
Caroline Viana <carolviana-21@hotmail.com>
2023-04-26 15:23:53 -0300

    :sparkles: Criando as telas de listar todos as categorias e produtos @Caroline-Viana

--- src/components/produtos/listaProdutos/ListaProdutos.tsx
+++ src/components/produtos/listaProdutos/ListaProdutos.tsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import './ListaProdutos.css'
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import Produto from '../../models/Produto';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import { busca } from '../../services/Service';
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import WifiProtectedSetupIcon from '@mui/icons-material/WifiProtectedSetup';
 
 function ListaProdutos() {
     const [produtos, setProdutos] = useState<Produto[]>([])
@@ -45,11 +48,28 @@ function ListaProdutos() {
                             <div className='Card_info'>
                                 <AttachMoneyIcon style={{ color: 'white' }} />
                                 <h2 className='Card_valor'>{`R$ ${produto.preco}`}</h2>
+
                             </div>
                         </div>
                         <div className='Card_criador'>
                             <img src="https://i.postimg.cc/SQBzNQf1/image-avatar.png" alt="avatar" className="small-avatar" />
                             <p className='Card_descricao'>Criado por <span className='nome_criador'>Jules Wyvern</span></p>
+                        </div>
+                        <div className='acoes'>
+                            <Link to={`/formularioProduto/${produto.id}`} className="text-decorator-none" >
+                                <div>
+                                    <IconButton aria-label="update">
+                                        <WifiProtectedSetupIcon />
+                                    </IconButton>
+                                </div>
+                            </Link>
+                            <Link to={`/deletarProduto/${produto.id}`} className="text-decorator-none">
+                                <div>
+                                    <IconButton aria-label="delete">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </div>
+                            </Link>
                         </div>
                     </div>
                 ))
