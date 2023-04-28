b46182df1030e0a8117e344f18e776fba51165c3
Caroline Viana <carolviana-21@hotmail.com>
2023-04-28 09:05:04 -0300

    :ok_hand: Arrumando o método put @Caroline-Viana

--- src/components/produtos/cadastroProduto/CadastroProduto.tsx
+++ src/components/produtos/cadastroProduto/CadastroProduto.tsx
@@ -83,7 +83,7 @@ function CadastroProduto() {
         e.preventDefault()
 
         if (id !== undefined) {
-            await put(`/produtos/${id}`, produto, setProduto, {
+            await put(`/produtos`, produto, setProduto, {
                 headers: {
                     'Authorization': token
                 }
